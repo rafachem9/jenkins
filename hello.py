24878cc2e0bbb818fb84dc0e4105080896745a96
rafachem9 <100517540+rafachem9@users.noreply.github.com>
2022-07-15 18:51:33 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 import os
+import requests
 
 print("hello world")
 print(os.getcwd())
+
+
+URL = "https://instagram.com/favicon.ico"
+# 2. download the data behind the URL
+response = requests.get(URL)
